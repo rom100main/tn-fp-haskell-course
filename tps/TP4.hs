081c75f438d4608ad0d16e9de1de1272894f3711
rom100main <rom100main@proton.me>
2025-10-20 11:16:11 +0200

    feat(TP4): eval expr, string to expr

--- tps/TP4.hs
+++ tps/TP4.hs
@@ -26,6 +26,50 @@ import Test.QuickCheck
 --   the result of evaluating your expressions with python3, using the 'pyEval'
 --   function below. Write a quickCheck test to compare the two implementations.
 
+data OpType
+  = Add
+  | Sub
+  | Mul
+  | Div
+  deriving (Eq, Enum, Show)
+
+data Expr
+  = Const Int
+  | Op OpType Expr Expr
+  deriving (Eq, Show)
+
+eval :: Expr -> Int
+eval (Const n) = n
+eval (Op Add e1 e2) = eval e1 + eval e2
+eval (Op Sub e1 e2) = eval e1 - eval e2
+eval (Op Mul e1 e2) = eval e1 * eval e2
+eval (Op Div e1 e2) = eval e1 `div` eval e2
+
+-- | Parse a string into an Expr.
+stringToExpr :: String -> Either String Expr
+stringToExpr s = parseExpr (words s)
+  where
+    parseExpr :: [String] -> Either String Expr
+    parseExpr [] = Right (Const 0)
+    parseExpr [n] = case reads n :: [(Int, String)] of
+      [(val, "")] -> Right (Const val)
+      _ -> Left ("Invalid constant: " ++ n)
+    parseExpr xs =
+      case break (`elem` ["*", "/"]) xs of
+        (left, op : right) | op `elem` ["*", "/"] -> do
+          leftExpr <- parseExpr left
+          rightExpr <- parseExpr right
+          let operator = if op == "*" then Mul else Div
+          Right (Op operator leftExpr rightExpr)
+        _ ->
+          case break (`elem` ["+", "-"]) xs of
+            (left, op : right) | op `elem` ["+", "-"] -> do
+              leftExpr <- parseExpr left
+              rightExpr <- parseExpr right
+              let operator = if op == "+" then Add else Sub
+              Right (Op operator leftExpr rightExpr)
+            _ -> Left "Invalid expression"
+
 -- | pyEval calls python3 and make it execute a python statement.
 -- For example, pyEval "2 * 3" returns "6"
 -- For 'readProcess' documentation, see:
@@ -43,3 +87,9 @@ main :: IO ()
 main = do
   pyResult <- pyEval "1 + 3"
   putStrLn ("pyEval \"1 + 3\" returned: " ++ pyResult)
+  let expr = Op Add (Const 1) (Const 3)
+  let result = eval expr
+  putStrLn ("eval of " ++ show expr ++ " returned: " ++ show (result))
+  let exprStr = "1 + 5 * 3"
+  let parsedExpr = stringToExpr exprStr
+  putStrLn ("stringToExpr \"" ++ exprStr ++ "\" returned: " ++ show parsedExpr)
